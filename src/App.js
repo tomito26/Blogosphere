3852ab80aef63eadbd158d27e482638e2fe20853
tomito26 <tommybwah@gmail.com>
2023-03-27 22:38:34 +0300

    added positon fixed on the navigation bar and also the reccommended topic section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import './App.css';
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import profilePhoto from "./assets/carlosjames.webp";
 import uiux from './assets/uiux.webp';
